bd58b3fe8264271a132475bba6d7733a17d8b6b9
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-12-09 23:36:42 -0500

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,6 @@
+#!/usr/bin/env bash
 # Install Nginx web server (w/ Puppet)
+exec {'Install nginx':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo "Holberton School" > /var/www/html/index.html && new_string="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "30i $new_string" /etc/nginx/sites-available/default && sudo service nginx start',
+  provider => shell,
+    }
