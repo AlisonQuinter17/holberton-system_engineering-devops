b11d59d0d99f6b24ded40ce204f0fb3593469867
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-01 21:52:58 -0500

    Json format Api info

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Export to JSON. """
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+	todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(sys.argv[1])).json()
+	users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(sys.argv[1])).json()
+	_name = users.get('username')
+	_id = users.get('id')
+
+	to_dict = {_id: [{"task": i.get('title'),
+				"completed": i.get('completed'),
+				"username": _name} for i in todos]}
+
+	with open("{}.json".format(sys.argv[1]), mode="w") as id_json:
+		json.dump(to_dict, id_json)
