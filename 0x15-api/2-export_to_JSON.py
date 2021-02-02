75f6adec71e564fb0b29386668877e09359dd915
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-01 21:54:55 -0500

    JSON format API information

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,17 +5,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-
-	todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
                          .format(sys.argv[1])).json()
-	users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                          .format(sys.argv[1])).json()
-	_name = users.get('username')
-	_id = users.get('id')
+    _name = users.get('username')
+    _id = users.get('id')
+    to_dict = {_id: [{"task": i.get('title'),
+                      "completed": i.get('completed'),
+                      "username": _name} for i in todos]}
 
-	to_dict = {_id: [{"task": i.get('title'),
-				"completed": i.get('completed'),
-				"username": _name} for i in todos]}
-
-	with open("{}.json".format(sys.argv[1]), mode="w") as id_json:
-		json.dump(to_dict, id_json)
+    with open("{}.json".format(sys.argv[1]), mode="w") as id_json:
+            json.dump(to_dict, id_json)
