3d450a30460e32f0155c5e774a7c4574b1ffd7d2
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-01 19:35:43 -0500

    Csv format API Info

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Export to CSV. """
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(sys.argv[1])).json()
+    _name = users.get('name')
+    _id = users.get('id')
+
+    with open('{}.csv'.format(sys.argv[1]), mode='w') as id_csv:
+        f = csv.writer(id_csv, quoting=csv.QUOTE_ALL)
+
+        for r in todos:
+            if _id == r.get('userId'):
+                f.writerow([_id, _name, r.get('completed'), r.get('title')])
