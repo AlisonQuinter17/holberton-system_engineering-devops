7c6a6672df034d37a0044aa38646521d2621559d
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-04 17:38:02 -0500

    Number of subscribers for a given subreddit.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of
+subscribers for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Return the total number of subscribers. """
+
+    sub = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit), headers={"User-Agent": "Custom"})
+
+    total_subscribers = sub.json().get("data").get("subscribers")
+
+    if (sub.status_code == 200):
+        return (total_subscribers)
+    else:
+        return (0)
