fbe092fde88e155d1221fb3acef8aba30765d96c
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-04 19:49:41 -0500

    10 hot post of subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ 10 hot posts. """
+    sub = requests.get("https://reddit.com/r/{}/hot.json".format(subreddit),
+                       headers={"User-Agent": "Custom"}, params={"limit": 10})
+
+    if (sub.status_code == 200):
+        for i in sub.json().get("data").get("children"):
+            print ("{}".format(i.get("data").get("title")))
+    else:
+        print (None)
