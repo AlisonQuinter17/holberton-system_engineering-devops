f189c9d67366702f11e02d569df77bce0bef72be
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-04 23:38:42 -0500

    Recurse it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -14,11 +14,9 @@ def recurse(subreddit, hot_list=[], after=""):
     if (sub.status_code == 200):
         for i in sub.json().get("data").get("children"):
             hot_list.append(i.get("data").get("title"))
-
         after = sub.json().get("data").get("after")
         if after is None:
             return hot_list
     else:
         return None
-
     return recurse(subreddit, hot_list, after)
