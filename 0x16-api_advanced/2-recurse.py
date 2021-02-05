1db38b93656603c28d485f6db3d75e37d28c0e53
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-02-04 23:09:32 -0500

    Recurse it

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Returns a list containing the titles of all hot articles for a given subreddit.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """ Recurse it! """
+    sub = requests.get("https://reddit.com/r/{}/hot.json?after={}"
+                       .format(subreddit, after),
+                       headers={"User-Agent": "Custom"})
+
+    if (sub.status_code == 200):
+        for i in sub.json().get("data").get("children"):
+            hot_list.append(i.get("data").get("title"))
+
+        after = sub.json().get("data").get("after")
+        if after is None:
+            return hot_list
+        recurse(subreddit, hot_list, after)
+    else:
+        return None
